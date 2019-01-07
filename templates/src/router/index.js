236b4fb3ea270ad732f9d0fe4d734ad97587acf1
cashely <290119516@qq.com>
2019-01-07 14:26:46 +0800

    管理界面初始化router

--- templates/src/router/index.js
+++ templates/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import Index from '@/components/Index'
 
 Vue.use(Router)
 
@@ -8,8 +10,11 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: Login
+    },
+    {
+      path: '/index',
+      component: Index
     }
   ]
 })
