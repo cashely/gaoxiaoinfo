c7f634a506097f2ba2e87230844893a60ad6c8f7
Unknown <310547343@qq.com>
2019-01-13 16:54:21 +0800

    文章发布日期修改

--- templates/src/components/Articles/Index.vue
+++ templates/src/components/Articles/Index.vue
@@ -20,7 +20,13 @@
     <div class="sub-container-box">
       <el-table @row-click="routerDetailAction" border size="small" stripe style="width: 100%" :data="pages">
         <el-table-column prop="title" label="标题"></el-table-column>
-        <el-table-column prop="publishDate" label="发布日期" width="150"></el-table-column>
+        <el-table-column
+      label="日期"
+      width="180">
+      <template slot-scope="scope">
+        <span style="margin-left: 10px">{{moment(scope.row.publishDate).format('YYYY-MM-DD') }}</span>
+      </template>
+    </el-table-column>
         <el-table-column prop="address" label="招聘地点" width="150"></el-table-column>
         <el-table-column prop="endDate" label="截止日期" width="150"></el-table-column>
       </el-table>
@@ -39,6 +45,7 @@
 </template>
 <script>
 import {getArtilesData, getArticlePageInfo, getProvinces} from '../../apis'
+import moment from 'moment'
 export default {
   data() {
     return {
@@ -99,6 +106,7 @@ export default {
         this.total = count
       })
     },
+    moment,
     routerDetailAction(row, event, column) {
       this.$router.push({
         path: `/index/articles/${row._id}`
